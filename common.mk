1d7c2c2ba40819c2251efc85476380196e3c8458
Elite <elitebitvavo@gmail.com>
2026-02-15 20:56:27 +0400

    temp

--- common.mk
+++ common.mk
@@ -278,3 +278,6 @@ PRODUCT_COPY_FILES += \
     $(COMMON_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_CFI_INCLUDE_PATHS += hardware/samsung_slsi/scsc_wifibt/wpa_supplicant_lib
+
+-include vendor/lineage-priv/keys/keys.mk
+include vendor/lineage-priv/keys/keys.mk
